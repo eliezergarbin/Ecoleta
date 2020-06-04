87e02f168af68738d5c027ea0f4b1b07394a7dc8
Eliezer Nascimento <elieserdariogarbin@gmail.com>
2020-06-04 11:10:32 -0300

    Adicionando leafletjs na tela de Create-point

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import {Link} from 'react-router-dom';
 import {FiArrowLeft} from 'react-icons/fi';
-import {} from 'react-leaflet'
+import {Map, TileLayer, Marker} from 'react-leaflet';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -59,6 +59,14 @@ const CreatePoint = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+
+                    <Map center={[-21.9371715, -50.5032108 ]} zoom={15}>
+                    <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[-21.9371715, -50.5032108]} />
+                    </Map>
 
                     <div className="field-group">
                         <div className="field">
